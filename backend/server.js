a7062e26f52ef4b6e04dd7fac768fea9c4651a45
Meenakshi-Dubbaka <meenakshi.dubbaka16@gmail.com>
2025-11-07 21:30:24 +0530

    added chnages

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 8000;
 app.use(
   cors({
-    origin: "tick-it-task-app.vercel.app",
+    origin: "https://tick-it-task-app.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
